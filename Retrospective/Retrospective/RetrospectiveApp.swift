f28caf882f32d7d66c07fdc814fc2cf611c09e75
rlarjsdn3 <rlarjsdn3@naver.com>
2025-05-11 14:03:21 +0900

    feat: 프로젝트 초기 세팅 및 플레이스홀더 파일 생성

---
+++ Retrospective/Retrospective/RetrospectiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetrospectiveApp.swift
+//  Retrospective
+//
+//  Created by 김건우 on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct RetrospectiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
