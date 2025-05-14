1151f82436635972486d2f3cd77b08caf5966fc5
rlarjsdn3 <rlarjsdn3@naver.com>
2025-05-14 16:27:51 +0900

    feat: TabView 버튼 색상 적용

--- Retrospective/Views/ContentView.swift
+++ Retrospective/Views/ContentView.swift
@@ -58,10 +58,8 @@ struct ContentView: View {
             } label: {
                 VStack(alignment:.center) {
                     Image(systemName: "house")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 30)
-                        .foregroundStyle(.label)
+                        .font(.title)
+                        .foregroundStyle(selected == .home ? .label : .secondary.opacity(0.55))
                 }
             }
             Spacer()
@@ -72,10 +70,8 @@ struct ContentView: View {
             } label: {
                 VStack(alignment:.center) {
                     Image(systemName: "magnifyingglass")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 30)
-                        .foregroundStyle(.label)
+                        .font(.title)
+                        .foregroundStyle(selected == .search ? .label : .secondary.opacity(0.55))
                 }
             }
             Spacer()
@@ -86,10 +82,8 @@ struct ContentView: View {
             } label: {
                 VStack(alignment:.center) {
                     Image(systemName: "chart.pie")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 30)
-                        .foregroundStyle(.label)
+                        .font(.title)
+                        .foregroundStyle(selected == .statistic ? .label : .secondary.opacity(0.55))
                 }
             }
             Spacer()
@@ -100,50 +94,20 @@ struct ContentView: View {
             } label: {
                 VStack(alignment:.center) {
                     Image(systemName: "gearshape")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 30)
-                        .foregroundStyle(.label)
+                        .font(.title)
+                        .foregroundStyle(selected == .setting ? .label : .secondary.opacity(0.55))
                 }
 
             }
             Spacer()
 
         }
-
         .frame(width: 350 , height: 80)
         .background(.background)
         .clipShape(RoundedRectangle(cornerRadius: 30))
         .shadow(color: .black.opacity(0.33), radius: 15, x: 5, y: 5)
-
     }
 
-    ///각 탭에서 보여지는 실제 화면
-
-    struct  HomeView: View {
-        var body: some View {
-            CategoryManagementView()//TODO: HomeView() 넣어주세요
-
-        }
-    }
-
-    struct SearchView: View {
-        var body: some View {
-            Test2_View()// searchView() 넣어주세요
-        }
-    }
-
-    struct statisticView: View {
-        var body: some View {
-            Test3_View()// statistiView() 넣어주세요
-        }
-    }
-
-    struct SettingView: View {
-        var body: some View {// settingView() 넣어주세요
-            Test4_View()
-        }
-    }
 }
 
 #Preview {
