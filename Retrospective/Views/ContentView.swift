66a5d895c63ccca9f3dfd501bdc0915face29caa
정소이 <soyijeong99@gmail.com>
2025-05-14 19:01:15 +0900

    conflict 수정_2

--- Retrospective/Views/ContentView.swift
+++ Retrospective/Views/ContentView.swift
@@ -28,7 +28,7 @@ struct ContentView: View {
                     CategoryManagementView() //TODO: HomeView() 넣어주세요
                         .tag(Tab.home)
 
-                    SearchView()// searchView() 넣어주세요
+                    SearchView() // searchView() 넣어주세요
                         .tag(Tab.search)
 
                     StatisticsView()
