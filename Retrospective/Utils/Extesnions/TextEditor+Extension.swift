fa0e8f97a2fbb17870b76f347f332a7e1c3a3c3f
정소이 <soyijeong99@gmail.com>
2025-05-15 13:19:42 +0900

    WritingView 관련 Extension 추가

---
+++ Retrospective/Utils/Extesnions/TextEditor+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  TextEditor+Extension.swift
+//  Retrospective
+//
+//  Created by 정소이 on 5/15/25.
+//
+
+import SwiftUI
+
+extension TextEditor {
+
+    /// placeholder와 문자열을 이용해 커스텀한 TextEditor를 보여줍니다.
+//    func customTextEditor(placeholder: String, text: Binding<String>) -> some View {
+//        self.modifier(CustomTextEditorStyle(placeholder: placeholder, text: text))
+//    }
+}
